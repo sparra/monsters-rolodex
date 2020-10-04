c3507fa01ff08d0a1438de97542a36956bb0a8c5
Salvador Parra <Salvador Parra@INSPIRON7D5>
2020-10-04 16:00:45 -0500

    Updates

---
+++ precache-manifest.4257cdcb88f413bdfd1f09177430bfe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6678ecec92e1a747114eee7c2007666",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ab9da9972f1254816d87",
+    "url": "/monsters-rolodex/static/css/main.95f4283f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9da9972f1254816d87",
+    "url": "/monsters-rolodex/static/js/main.f42f4bc7.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
